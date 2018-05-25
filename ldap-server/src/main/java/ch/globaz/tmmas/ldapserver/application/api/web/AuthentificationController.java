4aa6efef233bf897bb3da491b22201bb7c7a9a03
SCE <sce@gglobaz.ch>
2018-05-25 17:12:48 +0200

    implements auth controller

--- ldap-server/src/main/java/ch/globaz/tmmas/ldapserver/application/api/web/AuthentificationController.java
+++ ldap-server/src/main/java/ch/globaz/tmmas/ldapserver/application/api/web/AuthentificationController.java
@@ -1,7 +1,26 @@
 package ch.globaz.tmmas.ldapserver.application.api.web;
 
+import ch.globaz.tmmas.ldapserver.domain.UtilisateurService;
+import ch.globaz.tmmas.ldapserver.domain.model.UtilisateursLdap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.xml.ws.Response;
+
+@RestController("/auth")
 public class AuthentificationController {
+
+	@Autowired
+	UtilisateurService utilisateurService;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<UtilisateursLdap> autenticateAndGetUser(@RequestBody AuthentificationRequest authrequest){
+
+		return ResponseEntity.ok(utilisateurService.authenticate(authrequest.getNomUtilisateur(),authrequest
+				.getMotDePasse()));
+	}
 }
---
+++ ldap-server/src/main/java/ch/globaz/tmmas/ldapserver/application/api/web/AuthentificationRequest.java
@@ -0,0 +1,20 @@
+package ch.globaz.tmmas.ldapserver.application.api.web;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthentificationRequest {
+
+	private String nomUtilisateur;
+	private String motDePasse;
+
+	public AuthentificationRequest(String nomUtilisateur, String motDePasse) {
+		this.nomUtilisateur = nomUtilisateur;
+		this.motDePasse = motDePasse;
+	}
+
+	public AuthentificationRequest() {
+	}
+}
